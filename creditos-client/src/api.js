55d319fca3ab53512ee2c5682a8bbe924dd61c63
mzuniga <manuel-zuniga@deuxit.com>
2026-01-14 13:08:17 -0300

    fix: avoid localhost api in prod

--- creditos-client/src/api.js
+++ creditos-client/src/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_API_URL || "/api";
+const baseURL = import.meta.env.PROD
+    ? import.meta.env.VITE_API_URL_PROD || "/api"
+    : import.meta.env.VITE_API_URL || "http://localhost:3000/api";
 const api = axios.create({ baseURL });
 api.interceptors.request.use(config => {
     const token = localStorage.getItem("token");
